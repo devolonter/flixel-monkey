a2bc0109aa5c10007a859b846cce9dd6aba7a2b8
Arthur Bikmullin <devolonter@gmail.com>
2012-07-31 23:52:41 +0400

    PSS => PSM

---
+++ native/flixel.psm.cs
@@ -0,0 +1,26 @@
+
+namespace flixel {
+
+	class functions {	
+
+		public static int systemMillisecs() {
+			return 0;
+		}
+		
+		public static void showMouse() {
+		}
+		
+		public static void hideMouse() {
+		}
+		
+		public static bool isMobile() {
+			return true;
+		}
+		
+		public static void openURL(String url) {
+			
+		}
+
+	}
+
+}
--- native/flixel.pss.cs
+++
@@ -1,1 +0,0 @@
-namespace flixel {	class functions {			public static int systemMillisecs() {			return 0;		}				public static void showMouse() {		}				public static void hideMouse() {		}				public static bool isMobile() {			return true;		}				public static void openURL(String url) {					}	}}
